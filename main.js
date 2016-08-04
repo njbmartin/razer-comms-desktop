85605a59360a980cb4ae4ba3483472a783f4a284
Nicholas Martin <n.martin@movura.co.uk>
2016-08-04 17:39:47 +0100

    Initial version

---
+++ main.js
@@ -0,0 +1,75 @@
+const {app, Tray, Menu, BrowserWindow, ipcMain} = require('electron');
+const path = require('path');
+
+const iconPath = path.join(__dirname, 'favicon.ico');
+let appIcon = null;
+let win = null;
+
+app.on('ready', function(){
+  win = new BrowserWindow({show: true, frame:false, icon: "./favicon.ico"});
+  win.loadURL("https://web.comms.razerzone.com/");
+  appIcon = new Tray(iconPath);
+  var contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Item2',
+      submenu: [
+        { label: 'submenu1' },
+        { label: 'submenu2' }
+      ]
+    },
+    {
+      label: 'Item3',
+      type: 'radio',
+      checked: true
+    },
+    {
+      label: 'Toggle DevTools',
+      accelerator: 'Alt+Command+I',
+      click: function() {
+        win.show();
+        win.toggleDevTools();
+      }
+    },
+    { label: 'Quit',
+      accelerator: 'Command+Q',
+      selector: 'terminate:',
+      click: function() {
+        app.quit();
+      }
+    }
+  ]);
+  appIcon.on('click', () => {
+  win.isVisible() ? win.hide() : win.show()
+})
+  appIcon.setToolTip('This is my application.');
+  appIcon.setContextMenu(contextMenu);
+});
+
+ipcMain.on('minimize_mainWindow', (event, arg) => {
+  console.log(arg)  // prints "ping"
+  win.minimize();
+});
+
+ipcMain.on('maximize_mainWindow', (event, arg) => {
+  console.log(arg)  // prints "ping"
+  win.isMaximized() ? win.unmaximize() : win.maximize();
+});
+
+ipcMain.on('show_mainWindow', (event, arg) => {
+  console.log(arg)  // prints "ping"
+  win.show();
+});
+
+ipcMain.on('hide_mainWindow', (event, arg) => {
+  console.log(arg)  // prints "ping"
+  win.hide();
+});
+
+ipcMain.on('show-notification', (event, arg) => {
+    console.log(arg);
+    appIcon.displayBalloon({
+        icon: arg.icon,
+        title: arg.name,
+        content: arg.message
+    });
+});
